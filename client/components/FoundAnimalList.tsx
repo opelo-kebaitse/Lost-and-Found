84f95b2e06d4d6aa50993c6e1d7e57b6cbafe3db
Opelo Kebaitse <opelopatricia1@gmail.com>
2023-11-03 12:09:09 +1300

    show contact details-v1

--- client/components/FoundAnimalList.tsx
+++ client/components/FoundAnimalList.tsx
@@ -1,5 +1,7 @@
 import { getFoundAnimals } from '../apis/animals'
 import { useQuery } from '@tanstack/react-query'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+import { useAuth0 } from '@auth0/auth0-react'
 
 export default function FoundAnimals() {
   const {
@@ -23,7 +25,15 @@ export default function FoundAnimals() {
       </>
     )
   }
+  
+  const { getAccessTokenSilently } = useAuth0()
 
+  const handleContactClick =async () => {
+    const token = getAccessTokenSilently()
+    
+
+  }
+  
   return (
     <div>
       <h2>Found Animals</h2>
@@ -32,6 +42,8 @@ export default function FoundAnimals() {
           <div className="foundAnimal" key={foundAnimal.user_id}>
             <img src={foundAnimal.photo} alt={foundAnimal.species} />
             <p>Species: {foundAnimal.species}</p>
+            <IfAuthenticated><button onClick={handleContactClick}>See Contact Details</button></IfAuthenticated>
+            <IfNotAuthenticated><p>Login for more details</p></IfNotAuthenticated>
           </div>
         ))}
       </div>
