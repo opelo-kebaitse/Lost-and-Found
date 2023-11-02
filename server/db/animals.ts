ba1baf5b72065416098067422beae94e12f47b43
Opelo Kebaitse <opelopatricia1@gmail.com>
2023-11-03 11:05:43 +1300

    update unregistered get database call

--- server/db/animals.ts
+++ server/db/animals.ts
@@ -2,13 +2,12 @@ import connection from './connection.ts'
 
 import { LostAnimal, FoundAnimal} from '../../models/animals.ts'
 
-// const columns = ['name', 'species','photo']
 
 export async function getLostAnimals(db = connection): Promise<LostAnimal[]> {
-  return db('lost').select()
+  return db('lost').select('id', 'name', 'species', 'photo')
 }
 
 
 export async function getFoundAnimals(db = connection): Promise<FoundAnimal[]> {
-  return db('found').select()
+  return db('found').select('id', 'species', 'photo')
 }
